907975cc5ee4c05a80738d788c15de2ddfc4c899
Anthony Foy <anthony.foy@hotmail.com>
2023-02-09 10:50:27 +0000

    Added more monsters and items

--- items.js
+++ items.js
@@ -6,11 +6,46 @@ const lootArray = [
     },
     {
         weaponName: "Shield",
+        weaponLevel: 5,
+    },
+    {
+        weaponName: "Right Arm Guard",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Left Arm Guard",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Left Shoulder Guard",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Right Shoulder Guard",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Right Wrist Guard",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Left Wrist Guard",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Gloves",
+        weaponLevel: 1,
+    },
+    {
+        weaponName: "Chestplate",
         weaponLevel: 3,
     },
     {
-        weaponName: "Armour",
+        weaponName: "Leg Guards",
+        weaponLevel: 3,
+    },
+    {
+        weaponName: "Feet Guards",
         weaponLevel: 2,
-    }
-    
+    },
 ]
--- monsters.js
+++ monsters.js
@@ -1,28 +1,84 @@
 // Array of monsters to fight
 const monsterArray = [
     {
-        monsterName: "Liquid Snake",
-        monsterLevel: 5,
-    },
-    {
-        monsterName: "Revolver Ocelot",
-        monsterLevel: 4,
-    },
-    {
-        monsterName: "Sniper Wolf",
-        monsterLevel: 3,
-    },
-    {
-        monsterName: "Raven",
-        monsterLevel: 2,
-    },
-    {
-        monsterName: "Psycho Mantis",
+        monsterName: "Harry",
         monsterLevel: 1,
     },
     {
-        monsterName: "Grey Fox",
+        monsterName: "Larry",
+        monsterLevel: 2,
+    },
+    {
+        monsterName: "Barry",
+        monsterLevel: 3,
+    },
+    {
+        monsterName: "Garry",
+        monsterLevel: 4,
+    },
+    {
+        monsterName: "Carrey",
+        monsterLevel: 5,
+    },
+    {
+        monsterName: "Ben",
+        monsterLevel: 6,
+    },
+    {
+        monsterName: "Glenn",
+        monsterLevel: 7,
+    },
+    {
+        monsterName: "Gwen",
+        monsterLevel: 8,
+    },
+    {
+        monsterName: "Jen",
+        monsterLevel: 9,
+    },
+    {
+        monsterName: "Ken",
         monsterLevel: 10,
+    },
+    {
+        monsterName: "Len",
+        monsterLevel: 11,
+    },
+    {
+        monsterName: "Bill",
+        monsterLevel: 12,
+    },
+    {
+        monsterName: "Jill",
+        monsterLevel: 13,
+    },
+    {
+        monsterName: "Phil",
+        monsterLevel: 14,
+    },
+    {
+        monsterName: "Will",
+        monsterLevel: 15,
+    },
+    {
+        monsterName: "Brody",
+        monsterLevel: 16,
+    },
+    {
+        monsterName: "Cody",
+        monsterLevel: 17,
+    },
+    {
+        monsterName: "Dodi",
+        monsterLevel: 18,
+    },
+    {
+        monsterName: "Jody",
+        monsterLevel: 19,
+    },
+    {
+        monsterName: "Dave the Bastard",
+        monsterLevel: 20,
     },
 
 ]
